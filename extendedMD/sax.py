006cc50d969ee011b0526354ccc48f07b6c588b2
Maria Inês Silva <misilva73@gmail.com>
2019-01-13 17:16:05 +0000

    code - change windown normalization in sax

--- extendedMD/sax.py
+++ extendedMD/sax.py
@@ -7,10 +7,10 @@ def extract_sax_sequence(ts, win_size, paa_size, alphabet_size=3, z_threshold=0.
     """Applies the sax transformation to a 1-dim time series"""
     sax_sequence = []
     cuts = cuts_for_asize(alphabet_size)
+    ts_znorm = znorm(ts, z_threshold)
     for t in range(0, len(ts) - win_size + 1):
-        ts_win = ts[t:(t+win_size)]
-        ts_win_normalized = znorm(ts_win, z_threshold)
-        paa_rep = paa(ts_win_normalized, paa_size)
+        ts_win = ts_znorm[t:(t+win_size)]
+        paa_rep = paa(ts_win, paa_size)
         sax_word = ts_to_string(paa_rep, cuts)
         sax_sequence.append(sax_word)
     return sax_sequence
