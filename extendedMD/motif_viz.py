864d1f3b391439e6674ac2f087fe325aef299c71
Maria Inês Silva <maria.silva@feedzai.com>
2019-01-26 15:59:33 +0000

    code - add viz for manoeuvre events

--- extendedMD/motif_viz.py
+++ extendedMD/motif_viz.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 def create_motif_table(pattern_list, motif_point_list, mdl_cost_list, mean_dist):
@@ -24,45 +25,50 @@ def compute_motif_overlap_ratio(motif_point_list):
     return overlap_ratio_list
 
 
-def plot_single_motif(ts_1d, motif_index, motif_point_list, center_point_list, pattern_list, yaxis_label):
+def plot_single_motif(ts_1d, events_ts, motif_index, motif_point_list, center_point_list, pattern_list, yaxis_label):
     motif_pointers = motif_point_list[motif_index]
     center_pointers = center_point_list[motif_index]
     motif_pattern = pattern_list[motif_index]
+    event_df = pd.DataFrame([ts_1d, events_ts.values]).T.reset_index()
+    event_df.columns = ['index', 'var', 'event']
     # Plots:
     fig = plt.figure(figsize=(12,6))
     plt.suptitle(motif_pattern)
     #subplot 1
     plt.subplot(3,1,1)
-    plt.plot(ts_1d)
+    plt.plot(ts_1d, 'tab:gray')
     for temp_point in motif_pointers:
-        plt.plot(temp_point, ts_1d[temp_point], 'r')
+        plt.plot(temp_point, ts_1d[temp_point], 'tab:blue')
+    
     plt.ylabel(yaxis_label)
+    plt.ylim(min(ts_1d), max(ts_1d))
+    sns.scatterplot(x="index", y="var", hue="event", data=event_df[event_df['event']>0], legend=False,
+                    palette=sns.xkcd_palette(['red', 'tangerine', 'greenish yellow']))
     #subplot 2
     plt.subplot(3,1,2)
     for temp_point in motif_pointers:
-        plt.plot(ts_1d[temp_point])
+        plt.plot(ts_1d[temp_point], 'tab:blue')
     plt.ylabel(yaxis_label)
+    plt.ylim(min(ts_1d), max(ts_1d))
     #subplot 3
     plt.subplot(3,1,3)
-    plt.plot(ts_1d[center_pointers])
+    plt.plot(ts_1d[center_pointers], 'tab:blue')
     plt.ylabel(yaxis_label)
+    plt.ylim(min(ts_1d), max(ts_1d))
     return fig
 
-
-def plot_k_motifs(k, ts_1d, mdl_cost_list, motif_point_list, center_point_list, pattern_list):
+def plot_k_motifs(k, ts_1d, events_ts, mdl_cost_list, motif_point_list, center_point_list, pattern_list):
     sorted_index = np.argsort(mdl_cost_list)
     for motif_index in sorted_index[0:k]:
-        fig = plot_single_motif(ts_1d, motif_index, motif_point_list, center_point_list, pattern_list, 'pca time-series')
+        fig = plot_single_motif(ts_1d, events_ts, motif_index, motif_point_list, center_point_list, pattern_list, 'pca time-series')
         plt.show()
     return 'Plot completed'
 
 
-def plot_k_multdim_motifs(k, multidim_ts, mdl_cost_list, motif_point_list, center_point_list, pattern_list):
+def plot_k_multdim_motifs(k, multidim_ts, events_ts, mdl_cost_list, motif_point_list, center_point_list, pattern_list):
     sorted_index = np.argsort(mdl_cost_list)
     for motif_index in sorted_index[0:k]:
-        fig1 = plot_single_motif(multidim_ts.iloc[:,0].values, motif_index, motif_point_list,
-                                 center_point_list, pattern_list, multidim_ts.columns[0])
-        plt.show()
-        fig2 = plot_single_motif(multidim_ts.iloc[:,1].values, motif_index, motif_point_list,
-                                 center_point_list, pattern_list, multidim_ts.columns[1])
-        plt.show()
+        for column in multidim_ts.columns:
+            fig1 = plot_single_motif(multidim_ts[column].values, events_ts, motif_index, motif_point_list,
+                                     center_point_list, pattern_list, column)
+            plt.show()
