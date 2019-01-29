4e4fd2cde045b5c735db0b82cebfa2516c6114fc
Maria Inês Silva <maria.silva@feedzai.com>
2019-01-29 22:31:45 +0000

    code - add option for input 1-d time-series

--- extendedMD/emd.py
+++ extendedMD/emd.py
@@ -4,11 +4,15 @@ from extendedMD.sax import extract_sax_sequence
 from extendedMD.modified_bs import extract_modified_bs_sequence
 from extendedMD.modified_bs import generate_bs_pointers
 from extendedMD.motif_candidates import find_all_motif_candidates
+import pandas as pd
 
 
-def find_motifs_from_emd(multi_dim_ts, R, win_size, paa_size, alphabet_size, adaptive_break_points=True, z_threshold=0.01):
+def find_motifs_from_emd(ts, R, win_size, paa_size, alphabet_size, adaptive_break_points=True, z_threshold=0.01):
     """Returns the full list of motifs form a multi-dimensional time-series by running the extendedMD algorithm"""
-    ts_1d = extract_pca_ts(multi_dim_ts)
+    if isinstance(ts, pd.DataFrame):
+        ts_1d = extract_pca_ts(ts)
+    else:
+        ts_1d = ts
     sax_sequence = extract_sax_sequence(ts_1d, win_size, paa_size, alphabet_size, adaptive_break_points, z_threshold)
     bs_seq, bs_len = extract_modified_bs_sequence(sax_sequence)
     bs_point = generate_bs_pointers(bs_len, win_size)
