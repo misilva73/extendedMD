d49559e851132b6c6b8ab84c6a64c508adb70127
Maria Inês Silva <maria.silva@feedzai.com>
2019-02-01 20:54:28 +0000

    update license, requirements and setup

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 import setuptools
+import os
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open('requirements.txt') as fp:
+    install_requires = fp.read()
+
 setuptools.setup(
     name="extendedMD",
-    version="0.1",
+    version="0.7",
     author="Maria Ines Silva",
     author_email="misilva73@gmail.com",
     description="A small example package",
@@ -13,6 +17,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/misilva73/extendedMD",
     packages=setuptools.find_packages(),
+    install_requires=install_requires,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
